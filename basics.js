c507b901abeb76b8d72ec74f7455234199f87cbb
Helidhimmar <98799596+Helidhimmar@users.noreply.github.com>
2022-06-20 23:46:10 +0530

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App=()=>{
+  return (
+    <>
+    <MyName />
+    <h1 className="myclass">Hello World....{3+3}</h1>
+    <p>awesome...</p>
+    <img src="" alt=""/>
+    
+    </>
+  );
+};
+const MyName=()=>{
+return<h1>hyy my name is helly....</h1>
+};
+export default App;
+MyName();
